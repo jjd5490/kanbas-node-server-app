245c5abaf63f155f8ba8ed407a1fcd2744eb3e7d
jjd5490 <jamesdudek5@gmail.com>
2023-11-18 10:20:01 -0500

    first commit

---
+++ courses/routes.js
@@ -0,0 +1,44 @@
+import Database from "../Database/index.js";
+function CourseRoutes(app) {
+  app.get("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    const course = Database.courses.find((c) => c._id["$oid"] === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    res.send(course);
+  });
+
+  app.put("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = req.body;
+    Database.courses = Database.courses.map((c) =>
+      c._id["$oid"] === id ? { ...course } : c
+    );
+    res.send(course);
+  });
+
+  app.delete("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    Database.courses = Database.courses.filter((c) => c._id["$oid"] !== id);
+    res.sendStatus(204);
+  });
+
+  app.post("/api/courses", (req, res) => {
+    const course = {
+      ...req.body,
+      _id: { $oid: new Date().getTime().toString() },
+    };
+    Database.courses.push(course);
+    res.send(course);
+  });
+
+  app.get("/api/courses", (req, res) => {
+    const courses = Database.courses;
+    res.send(courses);
+  });
+}
+export default CourseRoutes;
