78d5c0dae1eb52524ca237a8a2515f7bb496d0e5
jjd5490 <jamesdudek5@gmail.com>
2023-11-18 11:19:00 -0500

    removed capital m

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import HelloRoutes from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import "dotenv/config";
 
